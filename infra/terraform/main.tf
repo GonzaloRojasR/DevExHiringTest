bd0ea7a51300ade3768d5e9bb8871c4eb020a479
GonzaloRojasR <gorojas92@gmail.com>
2025-04-15 21:22:19 -0400

    update tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -2,12 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-module "ecr" {
-  source   = "../modules/ecr"
-  ecr_name = var.ecr_name
-}
-
 module "dev" {
-  source       = "../modules/dev-cluster"
-  cluster_name = "gr-dev-cluster"
+  source    = "./environments/dev"
+  ecr_name  = var.ecr_name
 }
