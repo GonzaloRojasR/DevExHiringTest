fd846a84eec949023de79f44fe6c2d4c8d1e267f
GonzaloRojasR <gorojas92@gmail.com>
2025-04-15 21:23:45 -0400

    update tf

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -1,9 +1,9 @@
 module "ecr" {
-  source   = "../../modules/ecr"
+  source   = "../modules/ecr"
   ecr_name = var.ecr_name
 }
 
 module "eks" {
-  source       = "../../modules/dev-cluster"
+  source       = "../modules/dev-cluster"
   cluster_name = var.cluster_name
 }
