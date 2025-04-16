77fd03b95cc0deeca151e841c169aac217172e84
GonzaloRojasR <gorojas92@gmail.com>
2025-04-16 01:52:11 -0400

    update sa tf

--- infra/terraform/modules/dev-cluster/main.tf
+++ infra/terraform/modules/dev-cluster/main.tf
@@ -55,8 +55,6 @@ module "aws_auth" {
   source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
   version = "20.8.4"
 
-  cluster_name = module.eks.cluster_name
-
   manage_aws_auth_configmap = true
 
   aws_auth_users = [
