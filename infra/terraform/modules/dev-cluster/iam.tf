1218f821e9d15fe4d1f4e901350468868d9a1df7
GonzaloRojasR <gorojas92@gmail.com>
2025-04-16 20:35:47 -0400

    test

--- infra/terraform/modules/dev-cluster/iam.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_iam_user_policy_attachment" "eks_admin" {
-  user       = "gr-sa-terraform-ci"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSAdminPolicy"
-}
-
-resource "aws_iam_user_policy_attachment" "eks_cluster_admin" {
-  user       = "gr-sa-terraform-ci"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterAdminPolicy"
-}
